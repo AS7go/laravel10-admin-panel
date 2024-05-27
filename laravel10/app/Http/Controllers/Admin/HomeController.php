3cf1f3ad05b63531e70ec04f54328d8395ae5798
Aleksandr <sh17aleksandr@gmail.com>
2024-05-27 06:32:16 +0300

    menu and del

--- laravel10/app/Http/Controllers/Admin/HomeController.php
+++ laravel10/app/Http/Controllers/Admin/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Models\Post;
+use App\Models\Category;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
@@ -12,8 +13,11 @@ class HomeController extends Controller
     {
         // $posts_count = Post::all()->count();
         $posts_count = Post::count();
+        $categories_count = Category::count();
+
         return view('admin.home.index', [
-            'posts_count'=>$posts_count
+            'posts_count'=>$posts_count,
+            'categories_count'=>$categories_count,
         ]);
     }
 }
--- laravel10/resources/views/admin/home/index.blade.php
+++ laravel10/resources/views/admin/home/index.blade.php
@@ -29,12 +29,12 @@
                         <div class="inner">
                             <h3> {{$posts_count}} </h3>
 
-                            <p>Posts</p>
+                            <p>Статьи</p>
                         </div>
                         <div class="icon">
                             <i class="ion ion-bag"></i>
                         </div>
-                        <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                        <a href="{{route('post.index')}}" class="small-box-footer">Все статьи <i class="fas fa-arrow-circle-right"></i></a>
                     </div>
                 </div>
                 <!-- ./col -->
@@ -42,17 +42,17 @@
                     <!-- small box -->
                     <div class="small-box bg-success">
                         <div class="inner">
-                            <h3>53<sup style="font-size: 20px">%</sup></h3>
+                            <h3>{{$categories_count}}</h3>
 
-                            <p>Bounce Rate</p>
+                            <p>Категории</p>
                         </div>
                         <div class="icon">
                             <i class="ion ion-stats-bars"></i>
                         </div>
-                        <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                        <a href="{{route('category.index')}}" class="small-box-footer">Все категории <i class="fas fa-arrow-circle-right"></i></a>
                     </div>
                 </div>
-                <!-- ./col -->
+                {{-- <!-- ./col -->
                 <div class="col-lg-3 col-6">
                     <!-- small box -->
                     <div class="small-box bg-warning">
@@ -82,7 +82,7 @@
                         <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
                     </div>
                 </div>
-                <!-- ./col -->
+                <!-- ./col --> --}}
             </div>
             <!-- /.row -->
         
